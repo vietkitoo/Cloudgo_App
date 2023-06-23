b3436b009c47952cabfdc9c1a2a7a2d2a02ca4ce
Quoc Viet <110998166+vietkitoo@users.noreply.github.com>
2023-06-23 16:14:02 +0700

    @@ chong mat qua

--- lib/Screens/HomeScreen.dart
+++ lib/Screens/HomeScreen.dart
@@ -9,11 +9,18 @@ import 'package:cloudgo_app/Screens/CloudCHAT.dart';
 import 'package:cloudgo_app/Screens/CloudPOS.dart';
 import 'package:cloudgo_app/Screens/More....dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
+
+
     List names = [
       'CloudSALES',
       'CloudWORK',
@@ -22,8 +29,18 @@ class HomePage extends StatelessWidget {
       'CloudTELE',
       'CloudCHAT',
       'CloudPOS',
-      'More...'
+      'More...',
+      'CloudSALES',
+      'CloudWORK',
+      'CloudCAM',
+      'CloudCARE',
+      'CloudTELE',
+      'CloudCHAT',
+      'CloudPOS',
     ];
+    int value = names.length - 7;
+    int value2 = names.length;
+    int check = 7;
 
     List<Color> colors = const [
       Color(0xFF4FEC6F),
@@ -34,6 +51,13 @@ class HomePage extends StatelessWidget {
       Color(0xFFF32C2C),
       Color(0xFFEFC959),
       Color(0xFF86E5CA),
+      Color(0xFF4FEC6F),
+      Color(0xFF003A72),
+      Color(0xFFF68549),
+      Color(0xFF5DC6EA),
+      Color(0xFF86E5CA),
+      Color(0xFFF32C2C),
+      Color(0xFFEFC959),
     ];
 
     List<Icon> icons = const [
@@ -45,6 +69,13 @@ class HomePage extends StatelessWidget {
       Icon(Icons.chat, color: Colors.white, size: 30),
       Icon(Icons.monetization_on, color: Colors.white, size: 30),
       Icon(Icons.dashboard_customize, color: Colors.white, size: 30),
+      Icon(Icons.query_stats, color: Colors.white, size: 30),
+      Icon(Icons.work, color: Colors.white, size: 30),
+      Icon(Icons.camera_indoor_outlined, color: Colors.white, size: 30),
+      Icon(Icons.groups, color: Colors.white, size: 30),
+      Icon(Icons.support_agent, color: Colors.white, size: 30),
+      Icon(Icons.chat, color: Colors.white, size: 30),
+      Icon(Icons.monetization_on, color: Colors.white, size: 30),
     ];
 
     List nameScreen = [
@@ -56,92 +87,120 @@ class HomePage extends StatelessWidget {
       const CloudChat(),
       const CloudPos(),
       const More(),
+      const CloudSales(),
+      const CloudWORK(),
+      const CloudCAM(),
+      const CloudCARE(),
+      const CloudTELE(),
+      const CloudChat(),
+      const CloudPos(),
     ];
-
     return Scaffold(
       body: ListView(
         children: [
-          Container(
-            padding:
-            const EdgeInsets.only(top: 15, left: 15, right: 15, bottom: 10),
-            decoration: const BoxDecoration(
-              color: Colors.lightBlue,
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(20),
-                bottomRight: Radius.circular(20),
-              ),
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
+          SizedBox(
+            height: 300,
+            child: Stack(
               children: [
-                Container(
-                  margin: const EdgeInsets.only(top: 5, bottom: 20),
-                  width: 320,
-                  height: 45,
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: TextFormField(
-                    decoration: const InputDecoration(
-                      border: InputBorder.none,
-                      hintText: "Search...",
-                      hintStyle: TextStyle(
-                        color: Colors.black,
+                Positioned(
+                  top: 0,
+                  left: 0,
+                  right: 0,
+                  child: Container(
+                    height: 200,
+                    padding: const EdgeInsets.only(
+                        top: 15, left: 15, right: 15, bottom: 10),
+                    decoration: const BoxDecoration(
+                      color: Colors.lightBlue,
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(110),
+                        bottomRight: Radius.circular(110),
                       ),
-                      prefixIcon: Icon(
-                        Icons.search,
-                        size: 25,
-                      ),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.only(bottom: 120),
+                          width: 330,
+                          height: 45,
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: TextFormField(
+                            decoration: const InputDecoration(
+                              border: InputBorder.none,
+                              hintText: "Search...",
+                              hintStyle: TextStyle(
+                                color: Colors.black,
+                              ),
+                              prefixIcon: Icon(
+                                Icons.search,
+                                size: 25,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          width: 5,
+                        ),
+                        Container(
+                          margin: const EdgeInsets.only(bottom: 120),
+                          width: 35,
+                          height: 35,
+                          alignment: Alignment.center,
+                          decoration: const BoxDecoration(
+                            color: Color(0x647A7676),
+                            shape: BoxShape.circle,
+                          ),
+                          child: const Center(
+                            child: Icon(
+                              Icons.account_circle_outlined,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
-                Container(width: 5,),
-                Container(
-                  margin: const EdgeInsets.only(top: 5, bottom: 20),
-                  width: 35,
-                  height: 35,
-                  alignment: Alignment.center,
-                  decoration: const BoxDecoration(
-                    color: Color(0x647A7676),
-                    shape: BoxShape.circle,
-                  ),
-                  child: const Center(
-                    child: Icon(
-                      Icons.account_circle_outlined,
-                      color: Colors.white,
-                    ),
+                Positioned(
+                  top: 80,
+                  left: 15,
+                  right: 15,
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 220,
+                        width: double.infinity,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(20),
+                          child: AnotherCarousel(
+                            images: const [
+                              AssetImage("assets/images/4.png"),
+                              AssetImage("assets/images/5.png"),
+                              AssetImage("assets/images/3.jpg"),
+                            ],
+                            dotSize: 4,
+                            indicatorBgPadding: 5,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ],
             ),
           ),
-          const SizedBox(height: 5,),
-          Column(
-            children: [
-              SizedBox(
-                height: 220,
-                width: double.infinity,
-                child: AnotherCarousel(
-                  images: const [
-                    AssetImage("assets/images/4.png"),
-                    AssetImage("assets/images/5.png"),
-                    AssetImage("assets/images/3.jpg"),
-                  ],
-                  dotSize: 4,
-                  indicatorBgPadding: 5,
-                ),
-              ),
-            ],
-          ),
           Padding(
             padding: const EdgeInsets.only(top: 20, left: 15, right: 15),
             child: Column(
               children: [
-                GridView.builder(
-                  itemCount: names.length,
+                (check != value) ? GridView.builder(
+                  itemCount: value,
                   shrinkWrap: true,
                   physics: const NeverScrollableScrollPhysics(),
                   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
@@ -153,10 +212,69 @@ class HomePage extends StatelessWidget {
                       children: [
                         InkWell(
                           onTap: () {
-                            Navigator.push(
+                            if (index == 7) {
+                              setState(() {
+                                value = value2;
+                              });
+                            } else {
+                              Navigator.push(
                                 context,
                                 MaterialPageRoute(
-                                builder: (context) => nameScreen[index]),);
+                                    builder: (context) => nameScreen[index]),
+                              );
+                            }
+                            ;
+                          },
+                          child: Container(
+                            width: 50,
+                            height: 50,
+                            decoration: BoxDecoration(
+                              color: colors[index],
+                              shape: BoxShape.circle,
+                            ),
+                            child: Center(
+                              child: icons[index],
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 5),
+                        Text(
+                          names[index],
+                          style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 15,
+                            color: Colors.black.withOpacity(0.7),
+                          ),
+                        ),
+                      ],
+                    );
+                  },
+                )
+                : GridView.builder(
+                  itemCount: value2,
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 4,
+                    childAspectRatio: 1,
+                  ),
+                  itemBuilder: (context, index) {
+                    return Column(
+                      children: [
+                        InkWell(
+                          onTap: () {
+                            if (index == 7) {
+                              setState(() {
+                                value2 = value;
+                              });
+                            } else {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => nameScreen[index]),
+                              );
+                            }
+                            ;
                           },
                           child: Container(
                             width: 50,
@@ -205,39 +323,37 @@ class HomePage extends StatelessWidget {
                     ),
                   ],
                 ),
-                const SizedBox(height: 5),
+                const SizedBox(height: 10),
                 SizedBox(
-                  child: SizedBox(
-                    child: SingleChildScrollView(
-                      scrollDirection: Axis.vertical,
-                      padding: const EdgeInsets.all(12),
-                      child: Column(
-                        children: [
-                          Container(
-                            margin: const EdgeInsets.symmetric(vertical: 5),
-                            height: 170,
-                            child: ListView(
-                              scrollDirection: Axis.horizontal,
-                              children: <Widget>[
-                                Image.asset(
-                                  "assets/images/3.jpg",
-                                  width: 280,
-                                ),
-                                Container(width: 15),
-                                Image.asset(
-                                  "assets/images/1.jpg",
-                                  width: 280,
-                                ),
-                                Container(width: 15),
-                                Image.asset(
-                                  "assets/images/4.png",
-                                  width: 280,
-                                ),
-                                Container(width: 15),
-                              ],
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.vertical,
+                    padding: const EdgeInsets.all(12),
+                    child: ClipRRect(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        margin: const EdgeInsets.symmetric(vertical: 0),
+                        height: 180,
+                        child: ListView(
+                          scrollDirection: Axis.horizontal,
+                          children: <Widget>[
+                            Image.asset(
+                              "assets/images/3.jpg",
+                              width: 320,
                             ),
-                          ),
-                        ],
+                            Container(width: 15),
+                            Image.asset(
+                              "assets/images/1.jpg",
+                              width: 320,
+                            ),
+                            Container(width: 15),
+                            Image.asset(
+                              "assets/images/4.png",
+                              width: 320,
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ),
--- lib/Screens/More....dart
+++ lib/Screens/More....dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloudgo_app/Screens/HomeScreen.dart';
 
 class More extends StatefulWidget {
   const More({super.key});
